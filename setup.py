b5a9133059f03e28dbb27f634181ad4ea67fe336
Erik Price <github@erikprice.net>
2019-01-05 22:32:05 -0800

    1.0.0
    🚢,🛳,⛴ !

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os.path
 
 from setuptools import setup, find_packages
 
-__version__ = '1.0.0rc7'
+__version__ = '1.0.0'
 
 
 readme_path = os.path.join(os.path.dirname(__file__), 'README.rst')
