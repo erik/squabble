875f2bc8b12cd37346751d7c61f68fc130419e47
Erik Price <github@erikprice.net>
2019-01-09 09:13:23 -0800

    Finish functionality for RequireForeignKey

--- squabble/rules/require_foreign_key.py
+++ squabble/rules/require_foreign_key.py
@@ -1,10 +1,8 @@
 import re
 
 import pglast
-from pglast.enums import ConstrType
+from pglast.enums import AlterTableType, ConstrType
 
-import squabble.rule
-from squabble.lint import Severity
 from squabble.message import Message
 from squabble.rules import BaseRule
 
@@ -58,59 +56,85 @@ class RequireForeignKey(BaseRule):
 
     def enable(self, root_ctx, config):
         column_re = re.compile(config.get('column_regex', self._DEFAULT_REGEX))
-        columns = {}
 
-        # We want to check both columns that are part of CREATE TABLE s
-        root_ctx.register('CreateStmt', self._create_table(column_re, columns))
-        root_ctx.register('AlterTableStmt', self._alter_table(column_re, columns))
+        # Keep track of column_name -> column_def node so we can
+        # report a sane location for the warning when a new column
+        # doesn't have a foreign key.
+        missing_fk = {}
+
+        # We want to check both columns that are part of CREATE TABLE
+        # as well as ALTER TABLE ... ADD COLUMN
+        root_ctx.register(
+            'CreateStmt',
+            lambda _, node: _create_table_stmt(node, column_re, missing_fk))
+
+        root_ctx.register(
+            'AlterTableStmt',
+            lambda _, node: _alter_table_stmt(node, column_re, missing_fk))
 
         def _report_missing(ctx):
-            nonlocal columns
+            nonlocal missing_fk
 
-            for column, node in columns.items():
-                print('reporting missing:', column)
+            for column, node in missing_fk.items():
                 ctx.report(
                     self.MissingForeignKeyConstraint(col=column),
                     node=node)
 
         root_ctx.register_exit(_report_missing)
 
-    @squabble.rule.node_visitor
-    def _create_table(self, ctx, table_node, column_re, columns):
-        def _has_fk(constraints):
-            if constraints == pglast.Missing:
-                return False
 
-            return any(
-                e.contype == ConstrType.CONSTR_FOREIGN
-                for e in constraints
-            )
+def _create_table_stmt(table_node, column_re, missing_fk):
+    table_name = table_node.relation.relname.value
 
-        table_name = table_node.relation.relname.value
-        missing_fk = {}
+    for e in table_node.tableElts:
+        # Defining a column, may include an inline constraint.
+        if e.node_tag == 'ColumnDef':
+            if _column_needs_foreign_key(column_re, e):
+                key = '{}.{}'.format(table_name, e.colname.value)
+                missing_fk[key] = e
 
-        for e in table_node.tableElts:
-            # Defining a column, may include an inline constraint.
-            if e.node_tag == 'ColumnDef':
-                col_name = e.colname.value
-                if column_re.match(col_name) and not _has_fk(e.constraints):
-                    missing_fk[col_name] = e
+        # FOREIGN KEY (...) REFERENCES ...
+        elif e.node_tag == 'Constraint':
+            _remove_satisfied_foreign_keys(table_name, e, missing_fk)
 
-            # FOREIGN KEY (...) REFERENCES ...
-            elif e.node_tag == 'Constraint':
-                if e.contype != ConstrType.CONSTR_FOREIGN:
-                    continue
 
-                # Clear out any columns that we earlier identified as
-                # needing a foreign key.
-                for col_name in e.fk_attrs:
-                    missing_fk.pop(col_name.string_value, '')
+def _alter_table_stmt(node, column_re, missing_fk):
+    table_name = node.relation.relname.value
 
-        for column, node in missing_fk.items():
-            key = '{}.{}'.format(table_name, column)
-            columns[key] = node
+    for cmd in node.cmds:
+        if cmd.subtype == AlterTableType.AT_AddColumn:
+            col_name = cmd['def'].colname.value
+            if _column_needs_foreign_key(column_re, cmd['def']):
+                key = '{}.{}'.format(table_name, col_name)
+                missing_fk[key] = cmd['def']
 
-    @squabble.rule.node_visitor
-    def _alter_table(self, ctx, node, column_re, columns):
-        # TODO: write this part
-        pass
+        elif cmd.subtype in (AlterTableType.AT_AddConstraint,
+                             AlterTableType.AT_AddConstraintRecurse):
+            constraint = cmd['def']
+            _remove_satisfied_foreign_keys(table_name, constraint, missing_fk)
+
+
+def _remove_satisfied_foreign_keys(table_name, constraint, missing_fk):
+    # Nothing to do if this isn't a foreign key constraint
+    if constraint.contype != ConstrType.CONSTR_FOREIGN:
+        return
+
+    # Clear out any columns that we earlier identified as
+    # needing a foreign key.
+    for col_name in constraint.fk_attrs:
+        key = '{}.{}'.format(table_name, col_name.string_value)
+        missing_fk.pop(key, '')
+
+
+def _column_needs_foreign_key(column_re, column_def):
+    name = column_def.colname.value
+    if not column_re.match(name):
+        return False
+
+    if column_def.constraints == pglast.Missing:
+        return True
+
+    return not any(
+        e.contype == ConstrType.CONSTR_FOREIGN
+        for e in column_def.constraints
+    )
--- tests/sql/require_foreign_key.sql
+++ tests/sql/require_foreign_key.sql
@@ -1,7 +1,7 @@
 -- enable:RequireForeignKey
--- >>> {"line": 14, "column": 13, "message_id": "MissingPrimaryKey"}
---
---
+-- >>> {"line": 12, "column": 24, "message_id": "MissingForeignKeyConstraint"}
+-- >>> {"line": 19, "column": 46, "message_id": "MissingForeignKeyConstraint"}
+
 
 CREATE TABLE doesnt_match_column_regex(foo_id_not_a_references int);
 
@@ -17,32 +17,4 @@ ALTER TABLE alter_table_with_fk_later ADD COLUMN foo_id int;
 ALTER TABLE alter_table_with_fk_later ADD FOREIGN KEY (foo_id) REFERENCES foo(pk);
 
 ALTER TABLE alter_table_without_fk ADD COLUMN foo_id int;
-
-/*
-    ALTER TABLE
-    [{'RawStmt': {'stmt': {'AlterTableStmt': {'cmds': [{'AlterTableCmd': {'behavior': 0,
-        'def': {'ColumnDef': {'colname': 'bar',
-          'constraints': [{'Constraint': {'contype': 8,
-             'fk_del_action': 'a',
-             'fk_matchtype': 's',
-             'fk_upd_action': 'a',
-             'initially_valid': True,
-             'location': 35,
-             'pk_attrs': [{'String': {'str': 'key'}}],
-             'pktable': {'RangeVar': {'inh': True,
-               'location': 46,
-               'relname': 'fore',
-               'relpersistence': 'p'}}}}],
-          'is_local': True,
-          'location': 27,
-          'typeName': {'TypeName': {'location': 31,
-            'names': [{'String': {'str': 'baz'}}],
-            'typemod': -1}}}},
-        'subtype': 0}}],
-     'relation': {'RangeVar': {'inh': True,
-       'location': 12,
-       'relname': 'foo',
-       'relpersistence': 'p'}},
-     'relkind': 37}},
-   'stmt_len': 55}}]
-    */
+ALTER TABLE alter_table_without_fk ADD UNIQUE (foo_id);
